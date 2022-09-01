8e91806eacaca55129245f894e2f62dea52896e2
Uwe Fladrich <49554848+uwefladrich@users.noreply.github.com>
2022-09-01 09:31:36 +0200

    Support eORCA1 grid for NEMO (#2)
    * isort+black for orca.py
    * Check top_level in domain_cfg only when needed
    * Introduce eORCA1L75
    * isort+black for SE task code

--- ocp_tool/grids/orca.py
+++ ocp_tool/grids/orca.py
@@ -3,13 +3,14 @@ from netCDF4 import Dataset
 
 
 def _valid_subgrid(subgrid):
-    return subgrid in ('t', 'u', 'v')
+    return subgrid in ("t", "u", "v")
 
 
 class ORCA:
 
     _orca_names = {
-        (362, 292, 75): 'ORCA1L75',
+        (362, 292, 75): "ORCA1L75",
+        (360, 331, 75): "eORCA1L75",
     }
 
     def __init__(self, domain_cfg, masks=None):
@@ -18,64 +19,70 @@ class ORCA:
         self.masks = masks
 
         with Dataset(domain_cfg) as nc:
-            if not {'x', 'y'}.issubset(nc.dimensions):
-                raise RuntimeError(
-                    'Missing dimensions in NEMO domain config'
-                )
+            if not {"x", "y"}.issubset(nc.dimensions):
+                raise RuntimeError("Missing dimensions in NEMO domain config")
             if not {
-                'glamt', 'glamu', 'glamv', 'glamf',
-                'gphit', 'gphiu', 'gphiv', 'gphif',
-                'e1t', 'e1u', 'e1v', 'e1f',
-                'e2t', 'e2u', 'e2v', 'e2f',
-                'top_level',
+                "glamt",
+                "glamu",
+                "glamv",
+                "glamf",
+                "gphit",
+                "gphiu",
+                "gphiv",
+                "gphif",
+                "e1t",
+                "e1u",
+                "e1v",
+                "e1f",
+                "e2t",
+                "e2u",
+                "e2v",
+                "e2f",
             }.issubset(nc.variables):
-                raise RuntimeError(
-                    'Missing variables in NEMO domain config'
-                )
+                raise RuntimeError("Missing grid variable(s) in NEMO domain config")
             try:
                 self.name = self._orca_names[
                     (
-                        nc.dimensions['x'].size,
-                        nc.dimensions['y'].size,
-                        nc.dimensions['z'].size,
+                        nc.dimensions["x"].size,
+                        nc.dimensions["y"].size,
+                        nc.dimensions["z"].size,
                     )
                 ]
             except KeyError:
-                raise RuntimeError(
-                    'Unknown dimensions in NEMO domain config'
-                )
-        if self.masks is not None:
+                raise RuntimeError("Unknown dimensions in NEMO domain config")
+        if self.masks is None:
+            with Dataset(domain_cfg) as nc:
+                if 'top_level' not in nc.variables:
+                    raise RuntimeError("Missing 'top_level' variable in NEMO domain config")
+        else:
             with Dataset(self.masks) as nc:
-                if not {
-                    'tmaskutil', 'umaskutil', 'vmaskutil'
-                }.issubset(nc.variables):
-                    raise RuntimeError(
-                        'Missing variables in NEMO masks file'
-                    )
+                if not {"tmaskutil", "umaskutil", "vmaskutil"}.issubset(nc.variables):
+                    raise RuntimeError("Missing variables in NEMO masks file")
 
-    def cell_latitudes(self, subgrid='t'):
+    def cell_latitudes(self, subgrid="t"):
         if not _valid_subgrid(subgrid):
-            raise ValueError(f'Invalid NEMO subgrid: {subgrid}')
+            raise ValueError(f"Invalid NEMO subgrid: {subgrid}")
         with Dataset(self.domain_cfg) as nc:
-            return nc.variables[f'gphi{subgrid}'][0, ...].data
+            return nc.variables[f"gphi{subgrid}"][0, ...].data
 
-    def cell_longitudes(self, subgrid='t'):
+    def cell_longitudes(self, subgrid="t"):
         if not _valid_subgrid(subgrid):
-            raise ValueError(f'Invalid NEMO subgrid: {subgrid}')
+            raise ValueError(f"Invalid NEMO subgrid: {subgrid}")
         with Dataset(self.domain_cfg) as nc:
-            return nc.variables[f'glam{subgrid}'][0, ...].data
+            return nc.variables[f"glam{subgrid}"][0, ...].data
 
-    def cell_areas(self, subgrid='t'):
+    def cell_areas(self, subgrid="t"):
         if not _valid_subgrid(subgrid):
-            raise ValueError(f'Invalid NEMO subgrid: {subgrid}')
+            raise ValueError(f"Invalid NEMO subgrid: {subgrid}")
         with Dataset(self.domain_cfg) as nc:
-            return \
-                nc.variables[f'e1{subgrid}'][0, ...].data \
-                * nc.variables[f'e2{subgrid}'][0, ...].data
+            return (
+                nc.variables[f"e1{subgrid}"][0, ...].data
+                * nc.variables[f"e2{subgrid}"][0, ...].data
+            )
 
-    def cell_masks(self, subgrid='t'):
+    def cell_masks(self, subgrid="t"):
         if not _valid_subgrid(subgrid):
-            raise ValueError(f'Invalid NEMO subgrid: {subgrid}')
+            raise ValueError(f"Invalid NEMO subgrid: {subgrid}")
 
         def mask_borders(mask):
             mask[-1, :] = 1  # mask north-fold line
@@ -85,35 +92,31 @@ class ORCA:
         if self.masks is not None:
             with Dataset(self.masks) as nc:
                 mask = np.where(
-                    nc.variables[f'{subgrid}maskutil'][0, ...].data > 0, 0, 1
+                    nc.variables[f"{subgrid}maskutil"][0, ...].data > 0, 0, 1
                 )
                 mask_borders(mask)
                 return mask
 
         # Without a NEMO mask file, compute masks from top_level in domain_cfg
         with Dataset(self.domain_cfg) as nc:
-            tmask = np.where(
-                nc.variables['top_level'][0, ...].data == 0, 1, 0
-            )
-            if subgrid == 't':
+            tmask = np.where(nc.variables["top_level"][0, ...].data == 0, 1, 0)
+            if subgrid == "t":
                 mask_borders(tmask)
                 return tmask
-            elif subgrid == 'u':
-                umask = tmask \
-                        * tmask.take(
-                            range(1, tmask.shape[1]+1), axis=1, mode='wrap'
-                          )
+            elif subgrid == "u":
+                umask = tmask * tmask.take(
+                    range(1, tmask.shape[1] + 1), axis=1, mode="wrap"
+                )
                 mask_borders(umask)
                 return umask
-            elif subgrid == 'v':
-                vmask = tmask \
-                        * tmask.take(
-                            range(1, tmask.shape[0]+1), axis=0, mode='clip'
-                          )
+            elif subgrid == "v":
+                vmask = tmask * tmask.take(
+                    range(1, tmask.shape[0] + 1), axis=0, mode="clip"
+                )
                 mask_borders(vmask)
                 return vmask
 
-    def cell_corners(self, subgrid='t'):
+    def cell_corners(self, subgrid="t"):
         """For the ORCA grid and staggered subgrids, see
         NEMO reference manual, section 4 'Space Domain (DOM)'
 
@@ -127,23 +130,21 @@ class ORCA:
         +------------> i
         """
         if not _valid_subgrid(subgrid):
-            raise ValueError(f'Invalid NEMO subgrid: {subgrid}')
+            raise ValueError(f"Invalid NEMO subgrid: {subgrid}")
 
         with Dataset(self.domain_cfg) as nc:
-            if subgrid == 't':
-                lats = nc.variables['gphif'][0, ...].data
-                lons = nc.variables['glamf'][0, ...].data
-            elif subgrid == 'u':
-                lats = nc.variables['gphiv'][0, ...].data
-                lons = nc.variables['glamv'][0, ...].data
-            elif subgrid == 'v':
-                lats = nc.variables['gphiu'][0, ...].data
-                lons = nc.variables['glamu'][0, ...].data
+            if subgrid == "t":
+                lats = nc.variables["gphif"][0, ...].data
+                lons = nc.variables["glamf"][0, ...].data
+            elif subgrid == "u":
+                lats = nc.variables["gphiv"][0, ...].data
+                lons = nc.variables["glamv"][0, ...].data
+            elif subgrid == "v":
+                lats = nc.variables["gphiu"][0, ...].data
+                lons = nc.variables["glamu"][0, ...].data
 
         if lats.shape != lons.shape:
-            raise ValueError(
-                f'Incompatible lat/lon arrays in {self.domain_cfg}'
-            )
+            raise ValueError(f"Incompatible lat/lon arrays in {self.domain_cfg}")
 
         # Note that some corner lats/lons will be left undefined (set to an
         # invalid initial value), because we do not handle the north-fold
@@ -155,19 +156,19 @@ class ORCA:
         lat, lon = 0, 1
         nw, ne, se, sw = 0, 1, 2, 3
 
-        if subgrid == 't':
+        if subgrid == "t":
             corners[lat, ne, :, :] = np.roll(lats, 1, axis=1)
             corners[lon, ne, :, :] = np.roll(lons, 1, axis=1)
             corners[lat, se, 1:, :] = np.roll(lats[:-1, :], 1, axis=1)
             corners[lon, se, 1:, :] = np.roll(lons[:-1, :], 1, axis=1)
 
-        elif subgrid == 'u':
+        elif subgrid == "u":
             corners[lat, ne, :, :] = lats
             corners[lon, ne, :, :] = lons
             corners[lat, se, 1:, :] = lats[:-1, :]
             corners[lon, se, 1:, :] = lons[:-1, :]
 
-        elif subgrid == 'v':
+        elif subgrid == "v":
             corners[lat, ne, :-1, :] = np.roll(lats[1:, :], 1, axis=1)
             corners[lon, ne, :-1, :] = np.roll(lons[1:, :], 1, axis=1)
             corners[lat, se, :, :] = np.roll(lats, 1, axis=1)
--- ocp_tool/scriptengine_task.py
+++ ocp_tool/scriptengine_task.py
@@ -1,27 +1,27 @@
 import logging
+
 import numpy as np
 
-import ocp_tool as ocpt
-import ocp_tool.grids
 import ocp_tool.grib
+import ocp_tool.grids
 import ocp_tool.oasis
 
 try:
-    from scriptengine.tasks.core import Task, timed_runner
     from scriptengine.exceptions import ScriptEngineTaskRunError
+    from scriptengine.tasks.core import Task, timed_runner
 except (ModuleNotFoundError, ImportError) as e:
     logging.warning(
-        'The ScriptEngine module could not be loaded, which means '
-        'that the OCPTool ScriptEngine tasks will not be available. '
-        f'(the error was: {e})'
+        "The ScriptEngine module could not be loaded, which means "
+        "that the OCPTool ScriptEngine tasks will not be available. "
+        f"(the error was: {e})"
     )
 else:
 
     class OCPTool(Task):
 
         _required_arguments = (
-            'oifs_grid_type',
-            'oifs_mask_file',
+            "oifs_grid_type",
+            "oifs_mask_file",
         )
 
         def __init__(self, arguments):
@@ -72,186 +72,171 @@ else:
             #           x - extremely high resolution (ORCA036)
 
             oasis_grid_names = {
-                'TCO199': 'ICH',  # 2nd character to be inserted later
-                'TCO159': 'ICM',
-                'TL255': 'ILM',
-                'TCO95': 'ICL',
-                'TL159': 'ILL',
-                'TQ21': 'IQx',
-                'ORCA1L75t': 'NOTM',
-                'ORCA1L75u': 'NOUM',
-                'ORCA1L75v': 'NOVM',
-                'rnfm-atm': 'RNFA',
-                'amipfr': 'AMIP',
+                "TCO199": "ICH",  # 2nd character to be inserted later
+                "TCO159": "ICM",
+                "TL255": "ILM",
+                "TCO95": "ICL",
+                "TL159": "ILL",
+                "TQ21": "IQx",
+                "ORCA1L75t": "NOTM",
+                "ORCA1L75u": "NOUM",
+                "ORCA1L75v": "NOVM",
+                "eORCA1L75t": "NETM",
+                "eORCA1L75u": "NEUM",
+                "eORCA1L75v": "NEVM",
+                "rnfm-atm": "RNFA",
+                "amipfr": "AMIP",
             }
 
             # Construct final name for atm grid in OASIS
             def oifs_oasis_grid_name(grid_type, specifier):
-                return oasis_grid_names[grid_type][0] \
-                       + specifier + oasis_grid_names[grid_type][1:]
+                return (
+                    oasis_grid_names[grid_type][0]
+                    + specifier
+                    + oasis_grid_names[grid_type][1:]
+                )
 
             # OpenIFS grid(s)
-            oifs_grid_type = self.getarg('oifs_grid_type', context)
+            oifs_grid_type = self.getarg("oifs_grid_type", context)
             try:
-                oifs_grid = ocpt.grids.factory(oifs_grid_type)
+                oifs_grid = ocp_tool.grids.factory(oifs_grid_type)
             except NotImplementedError:
                 self.log_error(
-                    'Invalid OIFS grid type: '
+                    "Invalid OIFS grid type: "
                     f'{self.getarg("oifs_grid_type", context)}'
                 )
                 raise ScriptEngineTaskRunError
-            self.log_debug('Write OIFS grids to grids.nc')
-            ocpt.oasis.write_grid(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'L'),
+            self.log_debug("Write OIFS grids to grids.nc")
+            ocp_tool.oasis.write_grid(
+                name=oifs_oasis_grid_name(oifs_grid_type, "L"),
                 lats=oifs_grid.cell_latitudes(),
                 lons=oifs_grid.cell_longitudes(),
                 corners=oifs_grid.cell_corners(),
-                append=False
+                append=False,
             )
-            ocpt.oasis.write_grid(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'O'),
+            ocp_tool.oasis.write_grid(
+                name=oifs_oasis_grid_name(oifs_grid_type, "O"),
                 lats=oifs_grid.cell_latitudes(),
                 lons=oifs_grid.cell_longitudes(),
-                corners=oifs_grid.cell_corners()
+                corners=oifs_grid.cell_corners(),
             )
-            self.log_debug('Write OIFS areas to areas.nc')
-            self.log_debug(
-                f'OIFS grid area: {oifs_grid.cell_areas().sum():12.8e}'
-            )
-            ocpt.oasis.write_area(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'L'),
+            self.log_debug("Write OIFS areas to areas.nc")
+            self.log_debug(f"OIFS grid area: {oifs_grid.cell_areas().sum():12.8e}")
+            ocp_tool.oasis.write_area(
+                name=oifs_oasis_grid_name(oifs_grid_type, "L"),
                 areas=oifs_grid.cell_areas(),
-                append=False
+                append=False,
             )
-            ocpt.oasis.write_area(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'O'),
-                areas=oifs_grid.cell_areas()
+            ocp_tool.oasis.write_area(
+                name=oifs_oasis_grid_name(oifs_grid_type, "O"),
+                areas=oifs_grid.cell_areas(),
             )
 
-            oifs_mask_file = self.getarg('oifs_mask_file', context)
+            oifs_mask_file = self.getarg("oifs_mask_file", context)
             try:
-                oifs_masks = ocpt.grib.read(oifs_mask_file, ('lsm', 'cl'))
+                oifs_masks = ocp_tool.grib.read(oifs_mask_file, ("lsm", "cl"))
             except (FileNotFoundError, PermissionError):
-                self.log_error(
-                    f'Could not open OIFS mask file "{oifs_mask_file}"'
-                )
+                self.log_error(f'Could not open OIFS mask file "{oifs_mask_file}"')
                 raise ScriptEngineTaskRunError
             oifs_lsm = np.where(
-                np.logical_or(
-                    oifs_masks['lsm'] > 0.5, oifs_masks['cl'] > 0.5
-                ), 0, 1
+                np.logical_or(oifs_masks["lsm"] > 0.5, oifs_masks["cl"] > 0.5), 0, 1
             )
-            self.log_debug('Write OIFS masks to masks.nc')
-            ocpt.oasis.write_mask(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'L'),
+            self.log_debug("Write OIFS masks to masks.nc")
+            ocp_tool.oasis.write_mask(
+                name=oifs_oasis_grid_name(oifs_grid_type, "L"),
                 masks=oifs_lsm,
-                append=False
+                append=False,
             )
-            ocpt.oasis.write_mask(
-                name=oifs_oasis_grid_name(oifs_grid_type, 'O'),
-                masks=1-oifs_lsm
+            ocp_tool.oasis.write_mask(
+                name=oifs_oasis_grid_name(oifs_grid_type, "O"), masks=1 - oifs_lsm
             )
 
             # NEMO grid
-            nemo_grid_file = self.getarg('nemo_grid_file', context, default=None)
-            nemo_mask_file = self.getarg('nemo_mask_file', context, default=None)
+            nemo_grid_file = self.getarg("nemo_grid_file", context, default=None)
+            nemo_mask_file = self.getarg("nemo_mask_file", context, default=None)
             if nemo_grid_file is not None:
                 try:
-                    nemo_grid = ocpt.grids.factory(
-                        'ORCA', nemo_grid_file, nemo_mask_file
+                    nemo_grid = ocp_tool.grids.factory(
+                        "ORCA", nemo_grid_file, nemo_mask_file
                     )
                 except (FileNotFoundError, PermissionError):
-                    self.log_error(
-                        f'Could not open NEMO grid file "{nemo_grid_file}"'
-                    )
+                    self.log_error(f'Could not open NEMO grid file "{nemo_grid_file}"')
                     raise ScriptEngineTaskRunError
-                self.log_debug('Write NEMO grids, areas, masks')
+                self.log_debug("Write NEMO grids, areas, masks")
                 self.log_debug(
-                    'NEMO t-grid area: '
+                    "NEMO t-grid area: "
                     f'{nemo_grid.cell_areas(subgrid="t").sum():12.8e}'
                 )
                 self.log_debug(
-                    'NEMO u-grid area: '
+                    "NEMO u-grid area: "
                     f'{nemo_grid.cell_areas(subgrid="u").sum():12.8e}'
                 )
                 self.log_debug(
-                    'NEMO v-grid area: '
+                    "NEMO v-grid area: "
                     f'{nemo_grid.cell_areas(subgrid="v").sum():12.8e}'
                 )
-                for subgrid in ('t', 'u', 'v'):
-                    ocpt.oasis.write_grid(
-                        name=oasis_grid_names[nemo_grid.name+subgrid],
+                for subgrid in ("t", "u", "v"):
+                    ocp_tool.oasis.write_grid(
+                        name=oasis_grid_names[nemo_grid.name + subgrid],
                         lats=nemo_grid.cell_latitudes(subgrid=subgrid),
                         lons=nemo_grid.cell_longitudes(subgrid=subgrid),
-                        corners=nemo_grid.cell_corners(subgrid=subgrid)
+                        corners=nemo_grid.cell_corners(subgrid=subgrid),
                     )
-                    ocpt.oasis.write_area(
-                        name=oasis_grid_names[nemo_grid.name+subgrid],
-                        areas=nemo_grid.cell_areas(subgrid=subgrid)
+                    ocp_tool.oasis.write_area(
+                        name=oasis_grid_names[nemo_grid.name + subgrid],
+                        areas=nemo_grid.cell_areas(subgrid=subgrid),
                     )
-                    ocpt.oasis.write_mask(
-                        name=oasis_grid_names[nemo_grid.name+subgrid],
-                        masks=nemo_grid.cell_masks(subgrid=subgrid)
+                    ocp_tool.oasis.write_mask(
+                        name=oasis_grid_names[nemo_grid.name + subgrid],
+                        masks=nemo_grid.cell_masks(subgrid=subgrid),
                     )
 
             # Runoff-mapper grid
-            rnfm_grid = ocpt.grids.factory('F128')
-            self.log_debug('Write RNFM grid to grids.nc')
-            ocpt.oasis.write_grid(
-                name=oasis_grid_names['rnfm-atm'],
+            rnfm_grid = ocp_tool.grids.factory("F128")
+            self.log_debug("Write RNFM grid to grids.nc")
+            ocp_tool.oasis.write_grid(
+                name=oasis_grid_names["rnfm-atm"],
                 lats=rnfm_grid.cell_latitudes(),
                 lons=rnfm_grid.cell_longitudes(),
-                corners=rnfm_grid.cell_corners()
+                corners=rnfm_grid.cell_corners(),
             )
-            self.log_debug('Write RNFM areas to areas.nc')
-            self.log_debug(
-                f'RNFM grid area: {rnfm_grid.cell_areas().sum():12.8e}'
+            self.log_debug("Write RNFM areas to areas.nc")
+            self.log_debug(f"RNFM grid area: {rnfm_grid.cell_areas().sum():12.8e}")
+            ocp_tool.oasis.write_area(
+                name=oasis_grid_names["rnfm-atm"], areas=rnfm_grid.cell_areas()
             )
-            ocpt.oasis.write_area(
-                name=oasis_grid_names['rnfm-atm'],
-                areas=rnfm_grid.cell_areas()
-            )
-            self.log_debug('Write RNFM mask to masks.nc')
-            if self.getarg('rnfm_mask_file', context, default=None):
-                self.log_error(
-                    'Reading the RNFM mask from file is not implemented yet'
-                )
+            self.log_debug("Write RNFM mask to masks.nc")
+            if self.getarg("rnfm_mask_file", context, default=None):
+                self.log_error("Reading the RNFM mask from file is not implemented yet")
                 raise ScriptEngineTaskRunError
             else:
-                ocpt.oasis.write_mask(
-                    name=oasis_grid_names['rnfm-atm'],
-                    masks=np.zeros((rnfm_grid.nlats, rnfm_grid.nlons))
+                ocp_tool.oasis.write_mask(
+                    name=oasis_grid_names["rnfm-atm"],
+                    masks=np.zeros((rnfm_grid.nlats, rnfm_grid.nlons)),
                 )
 
             # AMIP Forcing-reader grid
-            amipfr_grid = ocpt.grids.factory(
-                'regular_latlon',
-                nlats=180,
-                nlons=360
-            )
-            self.log_debug('Write AMIP-FR grid to grids.nc')
-            ocpt.oasis.write_grid(
-                name=oasis_grid_names['amipfr'],
+            amipfr_grid = ocp_tool.grids.factory("regular_latlon", nlats=180, nlons=360)
+            self.log_debug("Write AMIP-FR grid to grids.nc")
+            ocp_tool.oasis.write_grid(
+                name=oasis_grid_names["amipfr"],
                 lats=amipfr_grid.cell_latitudes(),
                 lons=amipfr_grid.cell_longitudes(),
-                corners=amipfr_grid.cell_corners()
+                corners=amipfr_grid.cell_corners(),
             )
-            self.log_debug('Write AMIP-FR areas to areas.nc')
-            self.log_debug(
-                f'AMIP-FR grid area: {amipfr_grid.cell_areas().sum():12.8e}'
+            self.log_debug("Write AMIP-FR areas to areas.nc")
+            self.log_debug(f"AMIP-FR grid area: {amipfr_grid.cell_areas().sum():12.8e}")
+            ocp_tool.oasis.write_area(
+                name=oasis_grid_names["amipfr"], areas=amipfr_grid.cell_areas()
             )
-            ocpt.oasis.write_area(
-                name=oasis_grid_names['amipfr'],
-                areas=amipfr_grid.cell_areas()
-            )
-            self.log_debug('Write AMIP-FR mask to masks.nc')
-            if self.getarg('amipfr_mask_file', context, default=None):
+            self.log_debug("Write AMIP-FR mask to masks.nc")
+            if self.getarg("amipfr_mask_file", context, default=None):
                 self.log_error(
-                    'Reading the AMIP-FR mask from file is not implemented yet'
+                    "Reading the AMIP-FR mask from file is not implemented yet"
                 )
                 raise ScriptEngineTaskRunError
             else:
-                ocpt.oasis.write_mask(
-                    name=oasis_grid_names['amipfr'],
-                    masks=np.zeros((amipfr_grid.nlats, amipfr_grid.nlons))
+                ocp_tool.oasis.write_mask(
+                    name=oasis_grid_names["amipfr"],
+                    masks=np.zeros((amipfr_grid.nlats, amipfr_grid.nlons)),
                 )
