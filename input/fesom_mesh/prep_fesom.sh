589de52ea98d94cb903b453e8a422caf1c7aa5ef
Jan Streffing <jan.streffing@awi.de>
2023-03-13 07:11:29 +0100

    change of direction, using cdo interpolation instead of python search

---
+++ input/fesom_mesh/prep_fesom.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#exp: ./prep_fesom.sh core2 r720x361 ../openifs_input_default/ICMGGaackINIT
+
+mesh_name=$1
+regular_resolution=$2
+oifs_icmgg_file=$3
+
+module load cdo nco
+
+echo $mesh_name
+echo $regular_resolution
+
+cdo genycon,${regular_resolution} -selname,cell_area -setgrid,${mesh_name}_griddes_nodes.nc ${mesh_name}_griddes_nodes.nc weights_cell_area_${regular_resolution}.nc
+
+cdo -L -remap,${regular_resolution},weights_cell_area_${regular_resolution}.nc -selname,cell_area -setgrid,${mesh_name}_griddes_nodes.nc ${mesh_name}_griddes_nodes.nc ${mesh_name}_regular.nc
+
+ncap2 -O -s 'where(cell_area>0.) cell_area=0;' ${mesh_name}_regular.nc ${mesh_name}_oce.nc
+cdo setmisstoc,1 ${mesh_name}_oce.nc ${mesh_name}_land.nc
+
+cdo griddes $oifs_icmgg_file > griddes.txt
+cdo -setgrid,$oifs_icmgg_file -remapdis,griddes.txt ${mesh_name}_land.nc ${mesh_name}_oifs.nc
