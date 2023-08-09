2bd162dd874812ac4ad1cfe601a45378b1f0415b
Jan Streffing <jan.streffing@awi.de>
2023-08-09 23:21:19 +0200

    wip hooking in pyfesom2 ascii to netcdf

--- input/fesom_mesh/prep_fesom.sh
+++ input/fesom_mesh/prep_fesom.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 #exp: ./prep_fesom.sh core2 r3600x1801 ../openifs_input_default/ICMGGhf05INIT
 
-mesh_name=$1
-regular_resolution=$2
-oifs_icmgg_file=$3
+mesh_file=$1
+mesh_name=$2
+regular_resolution=$3
+oifs_icmgg_file=$4
 
 module load cdo nco
+echo "cdo genycon,${regular_resolution} -selname,cell_area -setgrid,${mesh_file} ${mesh_file} weights_cell_area_${regular_resolution}.nc
+"
+cdo genycon,${regular_resolution} -selname,cell_area -setgrid,${mesh_file} ${mesh_file} weights_cell_area_${regular_resolution}.nc
 
-cdo genycon,${regular_resolution} -selname,cell_area -setgrid,${mesh_name}_griddes_nodes.nc ${mesh_name}_griddes_nodes.nc weights_cell_area_${regular_resolution}.nc
-
-cdo -L -remap,${regular_resolution},weights_cell_area_${regular_resolution}.nc -selname,cell_area -setgrid,${mesh_name}_griddes_nodes.nc ${mesh_name}_griddes_nodes.nc ${mesh_name}_regular.nc
+cdo -L -remap,${regular_resolution},weights_cell_area_${regular_resolution}.nc -selname,cell_area -setgrid,${mesh_file} ${mesh_file} ${mesh_name}_regular.nc
 
 ncap2 -O -s 'where(cell_area>0.) cell_area=0;' ${mesh_name}_regular.nc ${mesh_name}_oce.nc
 cdo setmisstoc,1 ${mesh_name}_oce.nc ${mesh_name}_land.nc
